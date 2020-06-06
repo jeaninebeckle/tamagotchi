ae846ad53a5a335da06b2201c02411272c9f59e7
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-06 12:24:35 -0500

    play quadrant buttons working

--- src/javascripts/components/playQuadrant/play.js
+++ src/javascripts/components/playQuadrant/play.js
@@ -1,19 +1,36 @@
 import utils from '../../helpers/utils';
-import petData from '../../helpers/data/petData';
+// import petData from '../../helpers/data/petData';
 import './play.scss';
 
+let fun = 50;
+
 const buildPlayQuadrant = () => {
-  const allPets = petData.getPets();
   let domString = '<div class="playTime">';
 
-  allPets.forEach((pet) => {
-    domString += `<h3>${pet.name}</h3>`;
-  });
+  domString += `<h3>Play</h3>
+    <h4>Current strength level: ${fun}</h4>
+    <button id="superButton">Super Fun Time</button>
+    <button id="kindaButton">Kinda Fun Time</button>
+    `;
 
   domString += '</div>';
 
-
   utils.printToDom('#play', domString);
 };
+
+const clickSuperButtonEvent = (e) => {
+  e.preventDefault();
+  fun = Math.min(fun + 50, 100);
+  buildPlayQuadrant();
+};
+
+const clickKindaButtonEvent = (e) => {
+  e.preventDefault();
+  fun = Math.min(fun + 2, 100);
+  buildPlayQuadrant();
+};
+
+$('body').on('click', '#superButton', clickSuperButtonEvent);
+$('body').on('click', '#kindaButton', clickKindaButtonEvent);
 
 export default { buildPlayQuadrant };
