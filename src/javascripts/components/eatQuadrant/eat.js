11021eadc21ffb9628825c752ff93f33773de6f8
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-06 13:01:18 -0500

    eat quadrant buttons working

--- src/javascripts/components/eatQuadrant/eat.js
+++ src/javascripts/components/eatQuadrant/eat.js
@@ -1,18 +1,37 @@
 import utils from '../../helpers/utils';
 import './eat.scss';
-import petData from '../../helpers/data/petData';
+// import petData from '../../helpers/data/petData';
+
+let full = 100;
 
 const buildEatQuadrant = () => {
-  const allPets = petData.getPets();
-  let domString = '<div class="snackTime">';
+  let domString = '<div class="mealTime">';
 
-  allPets.forEach((pet) => {
-    domString += `<h3>${pet.name}</h3>`;
-  });
+  domString += `<h3>Eat</h3>
+    <h4>Current strength level: ${full}</h4>
+    <button id="healthyButton">Healthy Food</button>
+    <button id="junkButton">Junk Food</button>
+    `;
 
   domString += '</div>';
 
   utils.printToDom('#eat', domString);
 };
+
+const clickHealthyButtonEvent = (e) => {
+  e.preventDefault();
+  full = Math.min(full + 10, 100);
+  buildEatQuadrant();
+};
+
+const clickJunkButtonEvent = (e) => {
+  e.preventDefault();
+  full -= 3;
+  if (full < 0) full = 0;
+  buildEatQuadrant();
+};
+
+$('body').on('click', '#healthyButton', clickHealthyButtonEvent);
+$('body').on('click', '#junkButton', clickJunkButtonEvent);
 
 export default { buildEatQuadrant };
